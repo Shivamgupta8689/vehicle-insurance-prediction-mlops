73c5b06a35b950739293c53bcfe630ff45008d89
Shivamgupta8689 <shivamgupta86923@gmail.com>
2025-10-26 12:02:32 +0530

    add setup and puproject file

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Satyam Gupta",
+    author_email="satyamgupta8670@gmail.com",
+    packages=find_packages()
+)

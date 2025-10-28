3197e2479b05761b3aa0f9ff71b57f3ab08eba1c
Shivamgupta8689 <shivamgupta86923@gmail.com>
2025-10-29 00:21:30 +0530

    data base create abd model are stored

---
+++ src/configuration/aws_connection.py
@@ -0,0 +1,46 @@
+import boto3
+import os
+from src.constants import AWS_SECRET_ACCESS_KEY_ENV_KEY, AWS_ACCESS_KEY_ID_ENV_KEY, REGION_NAME
+
+
+class S3Client:
+
+    s3_client = None
+    s3_resource = None
+
+    def __init__(self, region_name=REGION_NAME):
+        """ 
+        This Class gets aws credentials from env_variable and creates a connection with S3 bucket 
+        (Works with AWS or any S3-compatible storage like MinIO)
+        """
+
+        if S3Client.s3_resource is None or S3Client.s3_client is None:
+            __access_key_id = os.getenv(AWS_ACCESS_KEY_ID_ENV_KEY)
+            __secret_access_key = os.getenv(AWS_SECRET_ACCESS_KEY_ENV_KEY)
+
+            if __access_key_id is None:
+                raise Exception(f"Environment variable: {AWS_ACCESS_KEY_ID_ENV_KEY} is not set.")
+            if __secret_access_key is None:
+                raise Exception(f"Environment variable: {AWS_SECRET_ACCESS_KEY_ENV_KEY} is not set.")
+
+            # 👇 Add MinIO endpoint here
+            endpoint_url = "http://localhost:9000"
+
+            S3Client.s3_resource = boto3.resource(
+                "s3",
+                aws_access_key_id=__access_key_id,
+                aws_secret_access_key=__secret_access_key,
+                region_name=region_name,
+                endpoint_url=endpoint_url
+            )
+
+            S3Client.s3_client = boto3.client(
+                "s3",
+                aws_access_key_id=__access_key_id,
+                aws_secret_access_key=__secret_access_key,
+                region_name=region_name,
+                endpoint_url=endpoint_url
+            )
+
+        self.s3_resource = S3Client.s3_resource
+        self.s3_client = S3Client.s3_client
